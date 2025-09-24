01f8de4b6a15a34a82f5f3a80a780b11fcc15f1b
elaraby <elaraby98123@gmail.com>
2025-09-24 03:12:41 +0300

    addet

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const body=document.querySelector('body');
 const btn=document.querySelector('.btn');
-const menu=document.querySelector('.menu');
+const menu = document.querySelector('.menu');
+const openMenu = document.querySelector('.open');
+
+
+openMenu.addEventListener('click',()=>{
+    menu.classList.toggle('close');
+})
+
+
+
+
+
 btn.addEventListener('click',()=>{
     body.classList.toggle('dark');
 })
+
